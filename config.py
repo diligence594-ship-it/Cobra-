53384b7225418af897b3d498a68221cb5847f591
mrstrangerpvt2024-cmyk <mrstrangerpvt2024@gmail.com>
2025-11-10 09:38:43 +0530

    Set default environment variables in config.py
    Updated default values for API_ID, API_HASH, OWNER_ID, and SUDO_USERS in config.py.

--- config.py
+++ config.py
@@ -3,18 +3,18 @@
 import os
 from os import getenv
 # ---------------R---------------------------------
-API_ID = int(os.environ.get("API_ID", ""))
+API_ID = int(os.environ.get("API_ID", "22470912"))
 # ------------------------------------------------
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "511be78079ed5d4bd4c967bc7b5ee023")
 # ----------------D--------------------------------
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 # -----------------A-------------------------------
 BOT_USERNAME = os.environ.get("")
 # ------------------X------------------------------
-OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+OWNER_ID = int(os.environ.get("OWNER_ID", "7678862761"))
 # ------------------X------------------------------
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "7678862761").split()))
 # ------------------------------------------------
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-100"))
 # ------------------------------------------------
